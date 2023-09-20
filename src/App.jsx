7a863c9394afdef0c9dcdfb9bbb5c311e16bd1af
jessik126 <jessik126@gmail.com>
2023-09-20 18:42:00 -0300

    Create and reuse card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from "./components/layout/Card";
 import First from './components/basics/First';
 import WithParameter from './components/basics/WithParameter';
 import Fragment from './components/basics/Fragment';
@@ -7,12 +8,20 @@ import Random from "./components/basics/Random";
 
 export default _ => (
     <div id="app">
-        <h1>React Fundaments 2</h1>
-        <Random min={2} max={8}></Random>
-        <Random min={2} max={8}></Random>
-        <Fragment />
-        <WithParameter title="Grades" studant="Peter" grade={9.8} />
-        <WithParameter title="" studant="Mary" grade={9.5} />
-        <First />
+        <h1>React Fundaments</h1>
+        <Card title="#04 - Randon">
+            <Random min={2} max={8}></Random>
+            <Random min={2} max={8}></Random>
+        </Card>
+        <Card title="#03 - Fragments">
+            <Fragment />
+        </Card>
+        <Card title="#02 - Grades">
+            <WithParameter title="Grades" studant="Peter" grade={9.8} />
+            <WithParameter title="" studant="Mary" grade={9.5} />
+        </Card>
+        <Card title="#01 - First">
+            <First />
+        </Card>
     </div>
 );
