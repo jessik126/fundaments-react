29c7ba8e64454527d9af0ccd5c085f50a3adff4b
jessik126 <jessik126@gmail.com>
2023-09-20 18:55:55 -0300

    Editing css - flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css"
 
 import Card from "./components/layout/Card";
 import First from './components/basics/First';
@@ -7,21 +8,23 @@ import Fragment from './components/basics/Fragment';
 import Random from "./components/basics/Random";
 
 export default _ => (
-    <div id="app">
+    <div className="App">
         <h1>React Fundaments</h1>
-        <Card title="#04 - Randon">
-            <Random min={2} max={8}></Random>
-            <Random min={2} max={8}></Random>
-        </Card>
-        <Card title="#03 - Fragments">
-            <Fragment />
-        </Card>
-        <Card title="#02 - Grades">
-            <WithParameter title="Grades" studant="Peter" grade={9.8} />
-            <WithParameter title="" studant="Mary" grade={9.5} />
-        </Card>
-        <Card title="#01 - First">
-            <First />
-        </Card>
+        <div className="Cards">
+            <Card title="#04 - Randon">
+                <Random min={2} max={8}></Random>
+                <Random min={2} max={8}></Random>
+            </Card>
+            <Card title="#03 - Fragments">
+                <Fragment />
+            </Card>
+            <Card title="#02 - Grades">
+                <WithParameter title="Grades" studant="Peter" grade={9.8} />
+                <WithParameter title="" studant="Mary" grade={9.5} />
+            </Card>
+            <Card title="#01 - First">
+                <First />
+            </Card>
+        </div>
     </div>
 );
