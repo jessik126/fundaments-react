801b96aaa53ca7774698bec8b509d0d4e526980b
jessik126 <jessik126@gmail.com>
2023-09-20 17:40:37 -0300

    Using react fragment

---
+++ src/components/basics/Fragment.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Fragment(){
+    return (
+        <React.Fragment>
+            <h2>Fragment</h2>
+            <p>Be carefull with this mistake</p>
+        </React.Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import React from 'react';
 
 import First from './components/basics/First';
 import WithParameter from './components/basics/WithParameter';
+import Fragment from './components/basics/Fragment';
 
 ReactDOM.render( 
   <div id="app">
     <First></First>
     <WithParameter title="Grades" studant="Peter" grade={9.8} />
     <WithParameter title="" studant="Mary" grade={9.5} />
+    <Fragment/>
   </div>, 
   document.getElementById('root'));
