bfe7168a396784df409fe936649682f1bb562ffc
jessik126 <jessik126@gmail.com>
2023-09-21 12:17:31 -0300

    Conditional Else (with component)

--- src/components/Conditional/UserInfo.jsx
+++ src/components/Conditional/UserInfo.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import If from './if'
+import If, { Else } from './if'
 
 export default props => {
     const user = props.user || {}
     return (
         <div>
-            <If test={user && user.name}>
+            {/* <If test={user && user.name}>
                 Welcome <strong> {user.name} </strong>
             </If>
             <If test={!user || !user.name}>
                 <strong> No </strong> name or user set to welcome 
+            </If> */}
+
+            <If test={user && user.name}>
+                Welcome <strong> {user.name} </strong>
+                <Else>
+                    <strong> No </strong> name or user set to welcome
+                </Else>
             </If>
+
         </div>
     )
 }
--- src/components/Conditional/if.js
+++ src/components/Conditional/if.js
@@ -1,7 +1,19 @@
-export default props =>{
-    if(props.test){
-        return props.children;
+export default props => {
+    const elseChild = props.children.filter(child => {
+        return child.type && child.type.name;
+    })[0]
+
+    const ifChildren = props.children.filter(child => {
+        return child != elseChild;
+    })
+
+    if (props.test) {
+        return ifChildren;
+    } else if (elseChild) {
+        return elseChild;
     } else {
-        return false
+        return false;
     }
 }
+
+export const Else = props => props.children;
