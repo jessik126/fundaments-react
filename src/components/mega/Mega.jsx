de1a8e49788613248292213a6bbf80f4d481e922
jessik126 <jessik126@gmail.com>
2023-10-10 12:18:06 -0300

    Lottery randon sort - fix v1

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -1,29 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default props => {
-    let [numbersGenerated, setNumbersGenerated] = useState([]);
-
-    let [amount, setChangeValue] = useState(props.amount)
-    function whenChange(e) {
-        setChangeValue(e.target.value);
-    }
-
 
     function generateNumbers() {
         var numbers = [];
         var i = 0, value;
-        while(i < amount) {
-            value = parseInt(Math.random() * (60 - 1) + 1)
+        while (i < amount) {
+            value = parseInt(Math.random() * (60+1 - 1)) + 1
             if (numbers.find(e => e === value) === undefined) {
                 numbers.push(value)
                 i++;
             }
         }
-        return numbers.sort(function(a, b){return a - b});
+        return numbers.sort(function (a, b) { return a - b }); // sort((n1, n2) => n1 - n2
     }
 
+
+    let [numbersGenerated, setNumbersGenerated] = useState([]);
+    let [amount, setAmount] = useState(props.amount)
+
+    function whenChange(e) {
+        setAmount(+e.target.value);
+    }
     function generate() {
-        numbersGenerated = setNumbersGenerated( generateNumbers() )
+        numbersGenerated = setNumbersGenerated(generateNumbers())
         console.log(numbersGenerated)
     }
 
@@ -31,9 +31,12 @@ export default props => {
         <div>
             <h2>Random Numbers to Mega</h2>
             <label>Amount: </label>
-            <input type="number" value={amount} onChange={whenChange}/>
+            <input type="number" value={amount} onChange={whenChange} />
             <button onClick={generate}>Generate</button>
             <p> {numbersGenerated.length > 0 ? "Numbers:" : ""} {numbersGenerated.toString()}</p>
         </div>
     )
+
+    // onChange={e => setAmount(+e.target.value)}
+    // onClick={_ => setNumbersGenerated(generateNumbers())}
 }
